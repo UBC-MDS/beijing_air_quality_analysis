8431a7cc6f3ae278416f3e609e82b80f27fcbd13
Junrong Zhu <junrongz@student.ubc.ca>
2021-11-25 16:55:45 -0800

    Debug Rscript and create results folder

--- src/hypothesis_testing_script.R
+++ src/hypothesis_testing_script.R
@@ -3,61 +3,68 @@
 
 "Hypothesis Testing
 
-Usage: src/beijing_air_quality.rmd --data=<data> --out_dir=<out_dir>
+Usage:src/hypothesis_testing_script.R --input=<input> --out_dir=<out_dir>
   
 Options:
---input=<input>        Path (including filename) to input the preprocessed data
---out_dir=<out_dir>  path to directory where the file should be saved
+--input=<input>       Path (including filename) to input the preprocessed data
+--out_dir=<out_dir>   Path to directory where the file should be saved
 " -> doc
 
-#input path example : here("data", "raw","PRSA_Data_20130301-20170228")
+#input path example : data/processed/processed_data.csv
+#Rscript src/hypothesis_testing_script.R --input=data/processed/processed_data.csv --out_dir=results
 
 library(docopt)
 library(tidyverse)
 library(infer)
-library(knitr)
 library(cowplot)
 library(here)
-set.seed(2021)
+
 
 opt <- docopt(doc)
 
 main <- function(input, out_dir){
-  
+ 
   #loading the preprocessed data
-  pm_data <- read_feather(input)
-  
+  pm_data <- read_csv(input)
+  set.seed(2021)
+
   #simulation based permutation test
-  null_distribution <- pm_data |> 
-    specify(PM2.5 ~ class) |> 
-    hypothesis(null = "independence") |> 
-    generate(reps = 2000, type = "permutate") |> 
+  null_distribution <- pm_data |>
+    specify(PM2.5 ~ class) |>
+    hypothesize(null = "independence") |>
+    generate(reps = 10, type = "permute") |>
     calculate(stat = "diff in medians",
-              order = c("time_B", "time_A"))
-  
+              order = c("time_A", "time_B"))
+
   #observed test statistic
-  obs_diff_median <- pm_data |> 
-    specify(PM2.5 ~ class) |> 
+  obs_diff_median <- pm_data |>
+    specify(PM2.5 ~ class) |>
     calculate(stat = "diff in medians",
-              order = c("time_B", "time_A"))
-  
+              order = c("time_A", "time_B"))
+
   #calculating p-value
-  p_value <- get_p_value(null_distribution, 
-                         obs_stat = obs_diff_median, 
+  p_value <- get_p_value(null_distribution,
+                         obs_stat = obs_diff_median,
                          direction = "less")
-  
+  print(p_value)
   #visualizing null distribution with shaded p-value
-  dist_visual <- visualizae(null_distribution, bins = 30) +
+  dist_visual <- visualize(null_distribution, bins = 50) +
     shade_p_value(obs_stat = obs_diff_median, direction = "less",
                   fill = "lightblue")
-  
+
   #save the p-value for analysis file
-  save(p_value, file = paste0(out_dir, "/hypothesis_testing_p_value.rds"))
+  #save(p_value, file = paste0(out_dir, "/hypothesis_testing_p_value.csv"))
+
+
+  if(!file.exists(out_dir)){
+    dir.create(out_dir)
+  }
   
   #save plot of null distribution
-  ggsave("null_distribution_plot", dist_visual, 
-         path = out_dir)
+  ggsave("null_distribution_plot.png", 
+         plot = dist_visual,
+         path = here(out_dir))
+  
 }
-
 
 main(opt[["--input"]], opt[["--out_dir"]])
